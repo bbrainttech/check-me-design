c6183f2d18cd049f30a99c6c7563d6eaa53ce282
braintech <120840671+bbrainttech@users.noreply.github.com>
2024-04-24 02:47:41 +0100

    import export for images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,21 @@
+import img_hero from "./hero-img.webp";
+import img_client1 from "./clients/1.webp";
+import img_client2 from "./clients/2.webp";
+
+import img_MedicalSpecialist from "./ui/MedicalSpecialist.webp";
+import img_BreastCancerWarriors from "./ui/BreastCancerWarriors.webp";
+
+import img_prompt1 from "./prompt/1.webp";
+import img_prompt2 from "./prompt/2.webp";
+import img_prompt3 from "./prompt/3.webp";
+
+export {
+  img_hero,
+  img_client1,
+  img_client2,
+  img_BreastCancerWarriors,
+  img_MedicalSpecialist,
+  img_prompt1,
+  img_prompt2,
+  img_prompt3,
+};
