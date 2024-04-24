b815c56ce680a8e78db810b392501d578caccc11
braintech <120840671+bbrainttech@users.noreply.github.com>
2024-04-24 03:22:47 +0100

    updated README file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { buttonVariants } from "./components/ui/button";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <p>An error occured</p>,
+    errorElement: (
+      <section className="box py-20 flex flex-col items-center justify-center">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium md:font-bold text-balance">
+          Nothing here so far.
+        </h1>
+        <div className="mt-10">
+
+        <Link to="/" className={buttonVariants({})}>
+          Home
+        </Link>
+        </div>
+      </section>
+    ),
   },
 ]);
 
