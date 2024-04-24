41f413574f4c961a50acf1599ba7d519d4e5aed1
braintech <120840671+bbrainttech@users.noreply.github.com>
2024-04-24 23:15:18 +0100

    adjusted images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,18 +1,29 @@
 import img_hero from "./hero-img.webp";
+import img_old_hero from "./old-hero-img.png";
+
+import img_francis from './reviews/Francis.png'
 import img_client1 from "./clients/1.webp";
 import img_client2 from "./clients/2.webp";
 
 import img_MedicalSpecialist from "./ui/MedicalSpecialist.webp";
 import img_BreastCancerWarriors from "./ui/BreastCancerWarriors.webp";
+import img_user1 from './ui/user-1.png'
+import img_user2 from './ui/user-2.png'
+import img_phone_screen from './ui/phone-screen.png'
 
 import img_prompt1 from "./prompt/1.webp";
 import img_prompt2 from "./prompt/2.webp";
 import img_prompt3 from "./prompt/3.webp";
 
 export {
+  img_user1,
+  img_user2,
   img_hero,
+  img_old_hero,
   img_client1,
   img_client2,
+  img_francis,
+  img_phone_screen,
   img_BreastCancerWarriors,
   img_MedicalSpecialist,
   img_prompt1,
